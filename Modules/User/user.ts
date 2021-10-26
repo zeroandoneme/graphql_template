6dea9dde022fcc5038b6ca9a3a2b7b683b73b6aa
Yara <y.rammal@zeroandone.me>
2021-10-26 16:05:37 +0300

    template fix

--- Modules/User/user.ts
+++ Modules/User/user.ts
@@ -1,64 +1,69 @@
-import { createModule, gql } from "graphql-modules";
-import { PubSub } from "graphql-subscriptions";
+import {createModule, gql} from "graphql-modules";
+import {PubSub} from "graphql-subscriptions";
 
 import {
-  getAllUsers as getAllUsersService,
-  addUser as addUserService,
+    getAllUsers as getAllUsersService,
+    addUser as addUserService,
 } from "./Services/user.service";
 
 const pubsub = new PubSub();
 
-export const userModule = createModule({
-  id: "user-module",
-  dirname: __dirname,
-  typeDefs: [
+
+const userTypeDefs = [
     gql`
-      #Types
-      type User {
-        name: String!
-        age: Int!
-      }
-      #Queries
-      type Query {
-        getAllUsers: [User!]!
-      }
-      #Mutations
-      type Mutation {
-        createUser(name: String!, age: Int!): User!
-      }
-      #Subscription
-      type Subscription {
-        userCreated: User!
-      }
+        #Types
+        type User {
+            name: String!
+            age: Int!
+        }
+        #Queries
+        extend type Query {
+            getAllUsers: [User!]!
+        }
+        #Mutations
+        extend type Mutation {
+            createUser(name: String!, age: Int!): User!
+        }
+        #Subscription
+        extend type Subscription {
+            userCreated: User!
+        }
     `,
-  ],
-  resolvers: {
+]
+
+const userResolvers = {
     Query: {
-      async getAllUsers(parent: any, args: any, request: any) {
-        console.log("Trying to get all users.");
-        const users = await getAllUsersService();
-        console.log("Got all users.");
-        return users;
-      },
+        async getAllUsers(parent: any, args: any, request: any) {
+            console.log("Trying to get all users.");
+            const users = await getAllUsersService();
+            console.log("Got all users.");
+            return users;
+        },
     },
     Mutation: {
-      async createUser(parent: any, args: any) {
-        const user = args;
-        console.log("Trying to add user.");
-        const newUser = await addUserService(user);
-        console.log("Successfully added user.");
+        async createUser(parent: any, args: any) {
+            const user = args;
+            console.log("Trying to add user.");
+            const newUser = await addUserService(user);
+            console.log("Successfully added user.");
 
-        console.log("Trying to publish an event.");
-        pubsub.publish("USER_CREATED", { userCreated: newUser });
-        console.log("Successfully published an event.");
+            console.log("Trying to publish an event.");
+            pubsub.publish("USER_CREATED", {userCreated: newUser});
+            console.log("Successfully published an event.");
 
-        return newUser;
-      },
+            return newUser;
+        },
     },
     Subscription: {
-      userCreated: {
-        subscribe: () => pubsub.asyncIterator(["USER_CREATED"]),
-      },
+        userCreated: {
+            subscribe: () => pubsub.asyncIterator(["USER_CREATED"]),
+        },
     },
-  },
+};
+
+export const userModule = createModule({
+    id: "user-module",
+    dirname: __dirname,
+    typeDefs: userTypeDefs,
+    resolvers: userResolvers
 });
